b94d639b0c3e2fc87ae798b766843dc619f552fb
Luna Zarco <lunaz@berkeley.edu>
2026-09-20 21:24:54 -0700

    done with hw2

---
+++ homework2/homework2.py
@@ -0,0 +1,91 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+'''
+The difference between Git, GitHub, and Git Bash is that Git is the actual software that allows for version control and tracking changes in code, 
+GitHub is a web-based platform for hosting and collaborating on Git repositories, and Git Bash is a command-line interface that allows users to interact with Git on Windows systems.
+'''
+# 2) What’s the difference between the terminal and the command line?
+'''
+Terminal is the application that interacts with the command line, while the command line is the interface where users can type and execute commands.
+'''
+# 3) How does Windows PowerShell differ from Git Bash?
+'''
+Windows powershell is a commandline shell and scripting language. Git Bash is a command line interface that allows users to interact with Git on Windows systems.
+'''
+# 4) What’s the difference between Anaconda, conda, and Python?
+'''
+Anaconda is python for data science. Conda is a package manager that comes with Anaconda. Python is a programming language that can be used for various purposes.
+'''
+# 5) What is VS Code? 
+'''
+VS Code is a fast coding editor for languages like python, C++, and more.
+'''
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+'''
+Jupiter notebook is tool used to write and run code in interactive blocks, while displaying text, math formulas, and visual charts in the same document.
+'''
+# 7) What does ~/ mean?
+'''
+Current user's home directory
+'''
+# 8) What’s the difference between an absolute path and a relative path?
+'''
+Absolute path starts from root directory and relative path is from the current working directory.
+'''
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+'''
+Asbolute: python_decal_fa26/luna/course_assignments/homework2
+Relative: course_assignments/homework2
+'''
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+'''
+The cd command since its change directory 
+'''
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+'''
+It would remove a file
+'''
+# 12) What do the following commands do?
+# git add
+# git commit
+# git push
+'''
+Prepares your changes to be saved. Permanently saves the changes into your local project history. Sends your locally committed snapshots up to a remote server (aka GitHUb)
+'''
+# 13) What's the difference between "git add ." and "git add <file>"?
+'''
+Stages all changes in the current directory. The <file> one stages only one specific file. 
+'''
+# 14) What do "git status" and "git log -1" do?
+'''Shows the current state of your project directory. The other shows the most recent commit in your project's history.
+'''
+# 15) What’s the difference between cloning a repository and pulling from it?
+'''
+Cloning copies a remote repository to your computer for the first time. Pulling downloads new changes into a repository you have already cloned.
+'''
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+'''
+It was probably for homework 1 when I was trying to do the += command for numbers but VS Code told me to do it someway and then in a lecture they mentioned that it was syntax error on VS Code so it makes sense. 
+'''
+# 17) What’s a question you still have? What’s something you’re confused about?
+'''
+I don't have any questions at the moment, I am confused on how some things work but I think it's because I am barely getting the hang of it. I'll probably know over time.
+'''
+# 18) Tell me a fun fact!
+'''
+I tried to learn how to code before in highschool but I gave up because it looked so hard for self study.
+'''
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+'''
+x=400
+y=500
+print(x+y)
+it'll show me 400+500=900 which is pretty cool that i dont have to manually do the math, it'll just do it for me.
+'''
